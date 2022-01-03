39a6f75beb71aeaa91e67deb596687d55d8972ce
Jabair <jabairk@gmail.com>
2022-01-03 16:31:14 -0500

    added sessions

--- index.js
+++ index.js
@@ -1,72 +1,210 @@
 const express = require('express');
-// const faker = require('faker');
 const req = require('express/lib/request');
+const session = require('express-session');
+//creates instance of an express application 
 const app = express();
+///////////ADD express sessions middle ware here//////////////
+app.use(
+    session({
+        secret: "random string",
+        resave: false,
+        saveUninitialized: true,
+        cookie: { secure: false },
+    })
+);
+
+//parser logic
+app.use(express.urlencoded({extended: true}));
+app.use(express.json())
+
+//declaring a port, can be any number, need process.env.PORT for deployment
 const port = process.env.PORT || 5400;
 
 
+///////////ROUTES/////////////////////////////////
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    // res.send("my server has started");
-    res.render("home")
+    // const session_username = req.session ? req.session.username : "user not set";
+    // res.render("index", {my_user: session_username});
+    let user = "";
+    let punctuation = "";
+    let invalid_login = "";
+
+    invalid_login = req.query.reason || null;
+
+    if ( req.session && req.session.username){
+        user = req.session.username;
+        punctuation = ",";
+    }
+    res.render("index", {my_user: user, punctuation: punctuation, invalid_login: invalid_login})
+})
+
+app.post("/signup", (req, res) => {
+    const valid_users = [
+        {"name": "jabair", "password": "jabair"}
+    ]
+    const user = req.body.username;
+    const pass = req.body.password;
+
+    const found_user = valid_users.find(usr => {
+        return usr.name == user && usr.password == pass;
+    });
+
+    if (found_user) {
+        req.session.username = user;
+        res.redirect("/home")
+    } else {
+        req.session.destroy(() => {
+            console.log("user-reset")
+        })
+        res.redirect("/?reason=invalid_user")
+    }
+    
+    
 })
 
 app.get("/home", (req, res) => {
-    res.render("home")
-})
+    if (req.session && req.session.username) {
+        user = req.session.username;
+        punctuation = ",";
+        res.render("home", {my_user: user, punctuation: punctuation});
+    } else{
+        res.redirect("/");
+    }
+    
+
+});
+
+
 
 app.get("/oregontrail", (req, res) => {
-    res.render("oregontrail")
+    if (req.session && req.session.username) {
+        user = req.session.username;
+        punctuation = ",";
+        res.render("oregontrail", {my_user: user});
+    } else{
+        res.redirect("/");
+    }
+    // res.render("oregontrail")
 })
 
 app.get("/oregontrail/bojack", (req, res) => {
-    // const name = request.params['name'];
-    res.render("bojack")
+    if (req.session && req.session.username) {
+        user = req.session.username;
+        punctuation = ",";
+        res.render("bojack", {my_user: user});
+    } else{
+        res.redirect("/");
+    }
+    // res.render("bojack")
 })
 
 app.get("/oregontrail/bojack/continue", (req, res) => {
-    // const name = request.params['name'];
-    res.render("bojack-continue")
+    if (req.session && req.session.username) {
+        user = req.session.username;
+        punctuation = ",";
+        res.render("bojack-continue", {my_user: user});
+    } else{
+        res.redirect("/");
+    }
+    
+    // res.render("bojack-continue")
 })
 
 app.get("/oregontrail/patricia", (req, res) => {
-    // const name = request.params['name'];
-    res.render("patricia")
+    if (req.session && req.session.username) {
+        user = req.session.username;
+        punctuation = ",";
+        res.render("patricia", {my_user: user});
+    } else{
+        res.redirect("/");
+    }
+    
+    // res.render("patricia")
 })
 
 app.get("/oregontrail/patricia/continue", (req, res) => {
-    // const name = request.params['name'];
-    res.render("patricia-continue")
+    if (req.session && req.session.username) {
+        user = req.session.username;
+        punctuation = ",";
+        res.render("patricia-continue", {my_user: user});
+    } else{
+        res.redirect("/");
+    }
+    
+    // res.render("patricia-continue")
 })
 
 
 app.get("/oregontrail/django", (req, res) => {
-    // const name = request.params['name'];
-    res.render("django")
+    if (req.session && req.session.username) {
+        user = req.session.username;
+        punctuation = ",";
+        res.render("django", {my_user: user});
+    } else{
+        res.redirect("/");
+    }
+    
+    // res.render("django")
 })
 
 app.get("/oregontrail/django/continue", (req, res) => {
-    // const name = request.params['name'];
-    res.render("django-continue")
+    if (req.session && req.session.username) {
+        user = req.session.username;
+        punctuation = ",";
+        res.render("django-continue", {my_user: user});
+    } else{
+        res.redirect("/");
+    }
+    
+    // res.render("django-continue")
 })
 
 
 
 app.get("/spacetrail", (req, res) => {
-    res.render("spacetrail")
+    if (req.session && req.session.username) {
+        user = req.session.username;
+        punctuation = ",";
+        res.render("spacetrail", {my_user: user});
+    } else{
+        res.redirect("/");
+    }
+    // res.render("spacetrail")
 })
 
 app.get("/thematrix", (req, res) => {
-    res.render("thematrix")
+    if (req.session && req.session.username) {
+        user = req.session.username;
+        punctuation = ",";
+        res.render("thematrix", {my_user: user});
+    } else{
+        res.redirect("/");
+    }
+    // res.render("thematrix")
 })
 
 app.get("/thematrix/redpill", (req, res) => {
-    res.render("redpill")
+    if (req.session && req.session.username) {
+        user = req.session.username;
+        punctuation = ",";
+        res.render("redpill", {my_user: user});
+    } else{
+        res.redirect("/");
+    }
+    // res.render("redpill")
 })
 
 app.get("/truth", (req, res) => {
-    res.render("truth")
+    if (req.session && req.session.username) {
+        user = req.session.username;
+        punctuation = ",";
+        res.render("truth", {my_user: user});
+    } else{
+        res.redirect("/");
+    }
+    // res.render("truth")
 })
 
 
